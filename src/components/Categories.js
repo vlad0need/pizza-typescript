018514a1dcd475181dd85fa7bfeb4115213e8989
vladovcharov <ov4lil@icloud.com>
2023-02-08 13:23:54 +0200

    2(create components UI)

---
+++ src/components/Categories.js
@@ -0,0 +1,16 @@
+
+function Categories(){
+    return(
+        <div class="categories">
+              <ul>
+                <li class="active">Усі</li>
+                <li>Мʼясні</li>
+                <li>Вегетеріанські</li>
+                <li>Гриль</li>
+                <li>Гострі</li>
+                <li>Закриті</li>
+              </ul>
+            </div>
+    )
+}
+export default Categories;
